c12c62b99d8cc77f28b0385899a896023abd00de
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-11-09 10:33:15 +0100

    add corpus

--- testSpacy.py
+++ testSpacy.py
@@ -1,7 +1,8 @@
 from pprint import pprint
 import spacy
-import fr_core_news_md
-nlp_fr = fr_core_news_md.load()
+import subprocess
+print(subprocess.getoutput("python -m spacy download fr_core_news_md"))
+nlp_fr = spacy.load('fr_core_news_md')
 text = (''' Le texte à enter''')
 doc = nlp_fr(text)
 pprint([(word.text, word.label_) for word in doc.ents])
