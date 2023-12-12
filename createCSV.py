22b6bf4b93bcb478d9ac1ae8716ff81f579488cc
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-12-12 15:21:31 +0100

    refactor code

--- createCSV.py
+++ createCSV.py
@@ -5,9 +5,110 @@ import pandas as pd
 import subprocess
 
 print(subprocess.getoutput("python -m spacy download fr_core_news_md"))
-
 # Chargement du modèle SpaCy pour le français
-nlp_fr = spacy.load('fr_core_news_md')
+nlp = spacy.load("fr_core_news_md")
+
+
+def read_text_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
+# Alias (à voir comment le gérer manuellemenent)
+def resolve_alias(characters):
+    alias_resolution = {
+        "John Traitor": "John",
+        "Sir Traitor": "John",
+        "Hari Seldon": "Hari",
+        "Mr Seldon": "Hari",
+        "Mme Seldon": "Hari"
+    }
+    resolved_characters = [alias_resolution.get(char, char) for char in characters]
+    return resolved_characters
+
+
+def extract_characters(doc):
+    characters = set()
+    for ent in doc.ents:
+        if ent.label_ == "PER":
+            characters.add(ent.text)
+    return characters
+
+
+def create_graph(characters):
+    G = nx.Graph()
+    for character in characters:
+        G.add_node(character)
+        # Ajout de l'attribut 'names' avec le nom du personnage
+        G.nodes[character]['names'] = character
+    return G
+
+
+def detect_co_occurrences(doc, G):
+    co_occurrences = {}
+    # Co-occurrences
+    for i, token in enumerate(doc):
+        if token.ent_type_ == "PER":
+            # 25 tokens de differences
+            for j in range(i + 1, min(i + 25, len(doc))):
+                if doc[j].ent_type_ == "PER":
+                    co_occurrence = (token.text, doc[j].text)
+                    if co_occurrence not in co_occurrences:
+                        co_occurrences[co_occurrence] = 0
+                    co_occurrences[co_occurrence] += 1
+
+    for co_occurrence, freq in co_occurrences.items():
+        character1, character2 = co_occurrence
+        weight = freq
+        # Poids de l'arête basé sur la fréquence de la co-occurrence
+        if G.has_node(character1) and G.has_node(character2):
+            # Ajout d'une arête entre les personnages en co-occurrence
+            G.add_edge(character1, character2, weight=weight)
+
+    return G
+
+
+def process_chapter(chapter_path):
+    text = read_text_file(chapter_path)
+    doc = nlp(text)
+
+    characters = extract_characters(doc)
+    resolved_characters = resolve_alias(characters)
+
+    # Création du graphe pour le chapitre
+    G = create_graph(resolved_characters)
+    # Détection de co-occurences
+    G = detect_co_occurrences(doc, G)
+
+    return G
+
+
+def process_corpus(corpus_folder, book_folders):
+    df_dict = {"ID": [], "graphml": []}
+
+    for book_folder in book_folders:
+        book_path = os.path.join(corpus_folder, book_folder)
+        chapters = os.listdir(book_path)
+        chapter_number = 0
+        for chapter_file in chapters:
+            chapter_path = os.path.join(book_path, chapter_file)
+
+            G = process_chapter(chapter_path)
+
+            # Génération du graphml
+            graphml = "".join(nx.generate_graphml(G))
+            if book_folder == "prelude_a_fondation":
+                df_dict["ID"].append(f"paf{chapter_number}")
+            else:
+                df_dict["ID"].append(f"lca{chapter_number}")
+            df_dict["graphml"].append(graphml)
+            chapter_number += 1
+
+    # Génération du CSV
+    df = pd.DataFrame(df_dict)
+    df.set_index("ID", inplace=True)
+    df.to_csv("./my_submission.csv")
+
 
 # Chemin vers le dossier contenant les fichiers texte
 corpus_folder = "corpus_asimov_leaderboard"
@@ -15,57 +116,5 @@ corpus_folder = "corpus_asimov_leaderboard"
 # Liste des sous-dossiers pour chaque livre
 book_folders = ["les_cavernes_d_acier", "prelude_a_fondation"]
 
-df_dict = {"ID": [], "graphml": []}
-
-for book_folder in book_folders:
-    book_path = os.path.join(corpus_folder, book_folder)
-    chapters = os.listdir(book_path)
-
-    for chapter_file in chapters:
-        chapter_path = os.path.join(book_path, chapter_file)
-
-        with open(chapter_path, 'r', encoding='utf-8') as file:
-            text = file.read()
-
-        # Création du graphe pour le chapitre
-        G = nx.Graph()
-        doc = nlp_fr(text)
-
-        # Alias (à voir comment le gérer manuellemenent)
-        alias_resolution = {
-            "John Traitor": "John",
-            "Sir Traitor": "John",
-            "Hari Seldon": "Hari",
-            "Mr Seldon": "Hari",
-            "Mme Seldon": "Hari"
-        }
-
-        for ent in doc.ents:
-            if ent.label_ == "PER":
-                character = ent.text
-                # Alias === PER ?
-                if character in alias_resolution:
-                    character = alias_resolution[character]
-                G.add_node(character)
-                # Ajout de l'attribut 'names' avec le nom du personnage
-                G.nodes[character]['names'] = character
-
-        # Co-occurrences
-        for i, token in enumerate(doc):
-            if token.ent_type_ == "PER":
-                # 25
-                for j in range(i + 1, min(i + 25, len(doc))):
-                    if doc[j].ent_type_ == "PER":
-                        # Ajout d'une arête entre les personnages en co-occurrence
-                        if G.has_node(token.text) and G.has_node(doc[j].text):
-                            G.add_edge(token.text, doc[j].text)
-
-        # Génération du graphml
-        graphml = "".join(nx.generate_graphml(G))
-        df_dict["ID"].append(f"{book_folder}_{chapter_file.split('.')[0]}")
-        df_dict["graphml"].append(graphml)
-
-# Création du DataFrame et exportation au format CSV
-df = pd.DataFrame(df_dict)
-df.set_index("ID", inplace=True)
-df.to_csv("./my_submission.csv")
+# Pour chaque fichier des sous-dossiers, convertie les données et les inclue dans un CSV
+process_corpus(corpus_folder, book_folders)
