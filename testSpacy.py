0f01222245a284385d483f9d5671bae62126fcd0
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-12-12 12:11:21 +0100

    add another verification

--- testSpacy.py
+++ testSpacy.py
@@ -7,4 +7,4 @@ with open('corpus_asimov_leaderboard/les_cavernes_d_acier/output.txt') as file:
     file_content = file.read()
 text = file_content
 doc = nlp_fr(text)
-pprint([(word.text, word.label_) for word in doc.ents if (word.label_ == 'PER' or word.label_ == 'MISC')])
+pprint([(word.text, word.label_, word.root.pos_) for word in doc.ents if (word.label_ in ['PER', 'MISC'] and word.root.pos_ == 'PROPN')])
