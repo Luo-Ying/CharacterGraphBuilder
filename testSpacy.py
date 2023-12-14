fe35778aaa1075dd621534ff0d2a43e5899ec11f
Jérôme Burgeat <burgeat84@hotmail.fr>
2023-12-14 08:03:39 +0100

    another test updated

--- testSpacy.py
+++ testSpacy.py
@@ -3,7 +3,8 @@ import spacy
 import subprocess
 print(subprocess.getoutput("python -m spacy download fr_core_news_md"))
 nlp_fr = spacy.load('fr_core_news_md')
-with open('corpus_asimov_leaderboard/les_cavernes_d_acier/output.txt') as file:
+# with open('corpus_asimov_leaderboard/les_cavernes_d_acier/chapter_1.txt.preprocessed') as file:
+with open('corpus_reformed/les_cavernes_d_acier/chapter_1.txt') as file:
     file_content = file.read()
 text = file_content
 doc = nlp_fr(text)
