c0ccd3917d95e25200aff0fdd1c23b90ec0b6d73
yluo <yluo@orkis.com>
2023-12-26 21:27:31 +0100

    Make lemmatisation for all files of corpus

--- removeEmptyWord.py
+++ removeEmptyWord.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 code_of_special_character = [8212, 8217]
 
@@ -6,10 +7,10 @@ code_of_special_character = [8212, 8217]
 tag_of_usefull_word = ['N', 'R', 'V', 'Z', 'W'] # N:noun, R:adverb, V:verb, Z:number, W:date, 
 
 # Remove the punctuations
-def remove_punctuations(modif_file):
+def remove_punctuations(file_to_read, file_to_write):
     
-    with open(modif_file, 'r', encoding='utf-8') as file_read:
-        with open(modif_file+"_whithout_punctuation.txt", 'w', encoding='utf-8') as file_write:
+    with open(file_to_read, 'r', encoding='utf-8') as file_read:
+        with open(file_to_write+"_whithout_punctuation.txt", 'w', encoding='utf-8') as file_write:
             line = file_read.readline()
             while line:
                 words = line.split()
@@ -17,13 +18,12 @@ def remove_punctuations(modif_file):
                     if len(words[0]) > 1 and not ( ord(words[0][0]) < 65 or (ord(words[0][0]) > 90 and ord(words[0][0]) < 97) or (ord(words[0][0]) > 122 and ord(words[0][0]) < 192) or ord(words[0][0]) in code_of_special_character ):
                         file_write.write(line)
                 line = file_read.readline()
-    
-    with open(modif_file, 'w') as file:
-        file.write('')
                 
-def remove_uselessWords(modif_file):
-    with open(modif_file+"_whithout_punctuation.txt", 'r', encoding='utf-8') as file_read:
-        with open(modif_file, 'w', encoding='utf-8') as file_write:
+                
+def remove_uselessWords(file_to_write):
+    
+    with open(file_to_write+"_whithout_punctuation.txt", 'r', encoding='utf-8') as file_read:
+        with open(file_to_write, 'w', encoding='utf-8') as file_write:
             line = file_read.readline()
             while line:
                 words = line.split()
@@ -32,11 +32,34 @@ def remove_uselessWords(modif_file):
                         file_write.write(line)
                 line = file_read.readline()
                 
-    os.remove(modif_file+"_whithout_punctuation.txt")
+    os.remove(file_to_write+"_whithout_punctuation.txt")
                 
 # Remove the usless words
-def lemmatisation(input_filename):
-    remove_punctuations(input_filename)
-    remove_uselessWords(input_filename)
+def lemmatisation(file_to_read, file_to_write):
+    remove_punctuations(file_to_read, file_to_write)
+    remove_uselessWords(file_to_write)
 
-lemmatisation('test.txt')
+def checkAndCreateFoldert(folder_path):
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+# lemmatisation('test.txt')
+def lemmatisationInCorpus():
+    
+    folder_freeling_result = "corpus/corpus_treated_by_Freeling/"
+    folder_lemmatisation_result = "corpus/corpus_treated_lemmatisationFreeling/"
+    
+    checkAndCreateFoldert(folder_lemmatisation_result)
+    
+    path = Path(folder_freeling_result)
+    subdirectories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+    for dir in subdirectories:
+        if os.path.exists(folder_lemmatisation_result + dir)==False: os.makedirs(folder_lemmatisation_result + dir)
+        files = os.listdir(folder_freeling_result + dir)
+        for file in files:
+            file_to_read = folder_freeling_result + dir + "/" + file
+            file_to_write = folder_lemmatisation_result + dir + "/" + file
+            lemmatisation(file_to_read, file_to_write)
+            
+lemmatisationInCorpus()
