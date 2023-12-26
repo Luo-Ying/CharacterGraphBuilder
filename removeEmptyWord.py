f4c6cfc080df603cd0d795e9b445978276e3f942
yluo <yluo@orkis.com>
2023-12-26 19:42:21 +0100

    Code for remove punctuation and useless word in a file

--- removeEmptyWord.py
+++ removeEmptyWord.py
@@ -2,23 +2,39 @@ import string
 
 code_of_special_character = [8212, 8217]
 
-def remove_punctuation(content):
-    
-    content_without_punctuation = ''.join([' ' if ( ord(char) < 65 or (ord(char) > 90 and ord(char) < 97) or (ord(char) > 122 and ord(char) < 192) or ord(char) in code_of_special_character ) else char for char in content])
+# unseless : C:conjunction, D:determiner, P:pronoun, I:interjection, F:punctuation
+tag_of_usefull_word = ['N', 'R', 'V', 'Z', 'W'] # N:noun, R:adverb, V:verb, Z:number, W:date, 
 
-    return content_without_punctuation
+# Remove the punctuations
+def remove_punctuations(modif_file):
+    
+    with open(modif_file, 'r', encoding='utf-8') as file_read:
+        with open(modif_file+"_whithout_punctuation.txt", 'w', encoding='utf-8') as file_write:
+            line = file_read.readline()
+            while line:
+                words = line.split()
+                if words: 
+                    if len(words[0]) > 1 and not ( ord(words[0][0]) < 65 or (ord(words[0][0]) > 90 and ord(words[0][0]) < 97) or (ord(words[0][0]) > 122 and ord(words[0][0]) < 192) or ord(words[0][0]) in code_of_special_character ):
+                        file_write.write(line)
+                line = file_read.readline()
+    
+    with open(modif_file, 'w') as file:
+        file.write('')
+                
+def remove_uselessWords(modif_file):
+    with open(modif_file+"_whithout_punctuation.txt", 'r', encoding='utf-8') as file_read:
+        with open(modif_file, 'w', encoding='utf-8') as file_write:
+            line = file_read.readline()
+            while line:
+                words = line.split()
+                if words: 
+                    if len(words[2]) != "" and words[2][0] in tag_of_usefull_word:
+                        file_write.write(line)
+                line = file_read.readline()
+                
+# Remove the usless words
+def lemmatisation(input_filename):
+    remove_punctuations(input_filename)
+    remove_uselessWords(input_filename)
 
-
-def lemmatisation(input_filename, output_filename):
-    
-    with open(input_filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-    
-    content_without_punctuation = remove_punctuation(content)
-    # print(content_without_punctuation)
-    
-    with open(output_filename, 'w', encoding='utf-8') as file:
-        file.write(content_without_punctuation)
-    
-
-lemmatisation('test.txt', 'test_output.txt')
+lemmatisation('test.txt')
