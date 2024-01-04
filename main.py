8c51235d97b45906db858c3e9a97876c3c65fb18
Jérôme Burgeat <burgeat84@hotmail.fr>
2024-01-04 22:33:48 +0100

    remove choice to make reformedFile

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from reformFile import reformFile
+from createCSV import createCSV
 from createCSV import createCSV
 from createGraphFromCSV import createGraphFromCSV
 from findEntityName import findEntityName
 from removeEmptyWord import lemmatisationInCorpus
-from buildTokens import buildTokensInFiles
+
 
 def main():
     findEntityName()
