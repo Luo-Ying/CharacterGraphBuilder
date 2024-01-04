7ded533abafb2e28b4032c9652c6294325bd4122
Jérôme Burgeat <burgeat84@hotmail.fr>
2024-01-04 22:33:35 +0100

    remove choice to make reformedFile

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from removeEmptyWord import lemmatisationInCorpus
 from buildTokens import buildTokensInFiles
 
 def main():
-    userOption = input("Reforme file? (y/n)")
-    if userOption == "y": reformFile()
-    # process_corpus()
-    # createGraphFromCSV()
     findEntityName()
     lemmatisationInCorpus()
     buildTokensInFiles()
